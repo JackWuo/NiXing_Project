2a5a8dab71e4bc652b6ae72dc9c963b82d1c1ab6
Wangshu <445266405@qq.com>
2020-06-30 01:34:16 +0800

    优化视角

---
+++ Assets/Scripts/player/FollowTarget.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class FollowTarget : MonoBehaviour {
+
+    private GameObject player;
+    public Vector3 targetPosion;
+    public float speed = 0.2f;
+    private void Start()
+    {
+    }
+
+    private void Update()
+    {
+        if(player==null)
+            player = GameObject.FindGameObjectWithTag("Player");
+
+        //   targetPosion = new Vector2( Mathf.Max( 6.63f,Mathf.Min(37, player.transform.position.x)-0.37f), Mathf.Max(3.48f,Mathf.Min(25.41f, player.transform.position.y- 1.52f)));
+        targetPosion = new Vector3( player.transform.position.x,  player.transform.position.y ,-3);
+        transform.DOMove(targetPosion,speed);
+       // if(transform.position.x)
+    }
+}
